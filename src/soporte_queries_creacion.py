847bcea5ce1b58963318de6a080796a3f722c11b
davfranco1 <david@francoonline.net>
2024-10-30 14:40:52 +0100

    Update Creación Tablas

--- src/soporte_queries_creacion.py
+++ src/soporte_queries_creacion.py
@@ -3,63 +3,65 @@
 
 # Centro hospitalario identificador de hosñitales y nombres 
 query_creation_hospitales = """
-create table if not exists hospitales (
-    ncodi INT primary key,
+CREATE TABLE IF NOT EXISTS hospitales (
+    ncodi INT PRIMARY KEY,
     name VARCHAR(300)
 );
 """
 
+
 # Tipos de hospityalizacion de los hospitales 
 query_creation_tipo_hosp = """
-create table if not exists tipo_hospitalizacion (
-    tipo_id SERIAL primary key,
-    nombre VARCHAR(100) unique not null
-    );
+CREATE TABLE IF NOT EXISTS tipo_hospitalizacion (
+    tipo_id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) UNIQUE NOT NULL
+);
 """
+
 
 # Gastos
 query_creation_gastos = """
-create table if not exists gastos (
-    gastos_id SERIAL primary key,
-    ncodi INT not null,
-    año INT not null,
-    totalcompra NUMERIC , 
-    producfarma NUMERIC , 
-    materialsani NUMERIC , 
-    implantes NUMERIC , 
-    restomateriasani NUMERIC , 
-    servcontratado NUMERIC , 
-    trabajocontratado NUMERIC , 
-    xrestocompras NUMERIC , 
-    variaexistencias NUMERIC , 
-    servexteriores NUMERIC , 
-    sumistro NUMERIC , 
-    xrestoserviexter NUMERIC , 
-    gastopersonal NUMERIC , 
-    sueldos NUMERIC , 
-    indemnizacion NUMERIC , 
-    segsocempresa NUMERIC , 
-    otrgassocial NUMERIC , 
-    dotaamortizacion NUMERIC , 
-    perdidadeterioro NUMERIC , 
-    xrestogasto NUMERIC , 
+CREATE TABLE IF NOT EXISTS gastos (
+    gastos_id SERIAL PRIMARY KEY,
+    ncodi INT NOT NULL,
+    año INT NOT NULL,
+    totalcompra NUMERIC,
+    producfarma NUMERIC,
+    materialsani NUMERIC,
+    implantes NUMERIC,
+    restomateriasani NUMERIC,
+    servcontratado NUMERIC,
+    trabajocontratado NUMERIC,
+    xrestocompras NUMERIC,
+    variaexistencias NUMERIC,
+    servexteriores NUMERIC,
+    sumistro NUMERIC,
+    xrestoserviexter NUMERIC,
+    gastopersonal NUMERIC,
+    sueldos NUMERIC,
+    indemnizacion NUMERIC,
+    segsocempresa NUMERIC,
+    otrgassocial NUMERIC,
+    dotaamortizacion NUMERIC,
+    perdidadeterioro NUMERIC,
+    xrestogasto NUMERIC,
     totcompragasto NUMERIC,
-    ncodi INT references hospitales(ncodi),
- 
+    FOREIGN KEY (ncodi) REFERENCES hospitales(ncodi)
 );
 """
 
 # Ingresos hospitalizaciones 
 query_creation_ingresos = """
-create table if not exists ingresos (
-    id_ingresos serial primary key,
-    ncodi INT not null,
+CREATE TABLE IF NOT EXISTS ingresos (
+    id_ingresos SERIAL PRIMARY KEY,
+    ncodi INT NOT NULL,
     particulares NUMERIC,
     aseguradoras NUMERIC,
     aseguradoras_enfermedad NUMERIC,
     aseguradoras_trafico NUMERIC,
     mutuas NUMERIC,
-    tipo_id INT not null,
-    ncodi references hospitales(ncodi),
-    tipo_id references tipo_hospitalizacion(tipo_id)
-);"""
+    tipo_id INT NOT NULL,
+    FOREIGN KEY (ncodi) REFERENCES hospitales(ncodi),
+    FOREIGN KEY (tipo_id) REFERENCES tipo_hospitalizacion(tipo_id)
+);
+"""
